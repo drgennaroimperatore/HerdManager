6427b3442914960bc701bcd4ef70ab276c41e7f4
Gennaro Imperatore <spikeg88@hotmail.it>
2020-03-02 15:27:36 +0000

    Initial commit

---
+++ app/src/main/java/com/ilri/herdmanager/ui/home/HomeFragment.java
@@ -0,0 +1,56 @@
+package com.ilri.herdmanager.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.ilri.herdmanager.MainActivity;
+import com.ilri.herdmanager.NewCaseActivity;
+import com.ilri.herdmanager.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private ImageButton mAddCaseButton;
+    private MainActivity mMainActivityReference;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText("Welcome To the Herd Manager");
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mAddCaseButton = (ImageButton) view.findViewById(R.id.add_case_Button);
+        mAddCaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToNewCaseActivityIntent = new Intent(getActivity(), NewCaseActivity.class);
+                startActivity(goToNewCaseActivityIntent);
+            }
+        });
+    }
+}
