5b7957ed79334a713d3331faaeda487585bb2d93
Gennaro Imperatore <spikeg88@hotmail.it>
2020-03-13 14:55:59 +0000

    further improvements on UI/3

---
+++ app/src/main/java/com/ilri/herdmanager/NewDynamicEventDialog.java
@@ -0,0 +1,28 @@
+package com.ilri.herdmanager;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class NewDynamicEventDialog extends DialogFragment {
+    public NewDynamicEventDialog(Context context) {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_new_dynamic_event,null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
--- app/src/main/java/com/ilri/herdmanager/ui/main/AddHerdDynamicFragment.java
+++ app/src/main/java/com/ilri/herdmanager/ui/main/AddHerdDynamicFragment.java
@@ -1,5 +1,7 @@
 package com.ilri.herdmanager.ui.main;
 
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -11,12 +13,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.ilri.herdmanager.NewDynamicEventDialog;
+import com.ilri.herdmanager.NewSignEventDialog;
 import com.ilri.herdmanager.R;
 
 public class AddHerdDynamicFragment extends Fragment {
 
     private AddHerdDynamicViewModel mViewModel;
+    private Button mAddEventButton;
 
     public static AddHerdDynamicFragment newInstance() {
         return new AddHerdDynamicFragment();
@@ -26,6 +32,27 @@ public class AddHerdDynamicFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.add_herd_dynamic_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mAddEventButton = view.findViewById(R.id.button_add_herd_dynamic);
+        mAddEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment dialogFragment = new NewDynamicEventDialog(getContext());
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+                if (prev != null) {
+                    ft.remove(prev);
+                }
+                ft.addToBackStack(null);
+
+                dialogFragment.show(ft, "dialog");
+
+            }
+        });
     }
 
     @Override
