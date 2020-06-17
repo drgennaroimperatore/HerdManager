1e5708bb580f4a14b20207afbe4cd4968dbca334
Gennaro Imperatore <spikeg88@hotmail.it>
2020-06-17 12:27:14 +0100

    beta version for ILRI

--- app/src/main/java/com/ilri/herdmanager/ui/dialogs/HowToUseDialog.java
+++ app/src/main/java/com/ilri/herdmanager/ui/dialogs/HowToUseDialog.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,4 +28,15 @@ public class HowToUseDialog extends DialogFragment
         return inflater.inflate(R.layout.dialog_how_to_use,null);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button close = view.findViewById(R.id.how_to_use_dialog_close_button);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
 }
